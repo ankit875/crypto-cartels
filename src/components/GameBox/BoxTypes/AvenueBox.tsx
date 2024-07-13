612b1ad064e3265813c81305d63bd1304029cc58
ankit.agrawal <ankit.agrawal@inmobi.com>
2024-07-13 12:43:32 +0530

    asset update

--- src/components/GameBox/BoxTypes/AvenueBox.tsx
+++ src/components/GameBox/BoxTypes/AvenueBox.tsx
@@ -1,16 +1,19 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import "./boxTypes.scss";
+import path from '../../../assets/Die_1.png'
 
 export const AvenueBox = ({
   name,
   color,
   id,
   price,
+  imageName,
 }: {
   name?: any;
   color?: any;
   id?: any;
   price?: any;
+  imageName?:any
 }) => {
   const getOrientation = () => {
     if ((id >= 1 && id <= 11) || (id >= 21 && id <= 31))
@@ -22,6 +25,8 @@ export const AvenueBox = ({
     <div className="avenue-box">
       <div className={getOrientation()}>
         <div className="box-color" style={{ backgroundColor: color }}></div>
+        <br />
+        <img src={path.replace("Die_1.png",imageName)} width={20} height={20} />
         <div className="box-text">
           {name} <br />
         </div>
